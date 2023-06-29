8f8a759801650bb1c0dfd4d1626f21f5276e1eb8
jayan <jayududakiya@gmail.com>
2023-06-29 09:28:40 +0530

    Example of Constructor

---
+++ oops/EXconstructor.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Tast 
+{
+
+    public:
+   Tast()
+   {
+    cout<<"Defualt Constructor type call.... "<<endl;
+   }
+
+   Tast(int a) //// using int dataType 
+   {
+    cout<<"Perameterized Constructor type call.. A : "<<a<<endl;
+   }
+
+   Tast(char ch) // using char dataType 
+   {
+    cout<<"Perameterized Constructor type call.. ch : "<<ch<<endl;
+   }
+ 
+};
+
+int main()
+{
+    Tast t,b(23),c('J');
+}
